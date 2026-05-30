4207b0f4929b7f610277ee5dc4c01c0a861cbfd6
vyshnavichinni75 <vyshnavic76@gmail.com>
2026-05-30 19:11:30 +0530

    Create design.v

---
+++ Day_17/design.v
@@ -0,0 +1,18 @@
+module sr_ff(
+    input s,
+    input r,
+    input clk,
+    output reg q
+);
+always @(posedge clk)
+begin
+    if(s == 1'b0 && r == 1'b0)
+        q <= q;
+    else if(s == 1'b0 && r == 1'b1)
+        q <= 1'b0;
+    else if(s == 1'b1 && r == 1'b0)
+        q <= 1'b1;
+    else
+        q <= 1'bx;
+end
+endmodule
